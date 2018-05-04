237511e752bf13154ae41a716a46e55894f25655
thanuphon <thanuphon@thinknet.co.th>
2018-05-04 15:00:16 +0700

    delete hello

--- functions/index.js
+++ functions/index.js
@@ -9,9 +9,6 @@ admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
 
 exports.getMessageFromLine = functions.https.onRequest(middleware(config),(request, response) => {
   const strDate = moment().zone('+0700').format('YYYY-MM-DD');
