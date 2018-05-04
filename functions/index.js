18cd0d6cf5ac3083e746d013980a5d3e1af2fb61
thanuphon <thanuphon@thinknet.co.th>
2018-05-04 14:43:10 +0700

    get

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.get = functions.https.onRequest((request, response) => {
+  var config = {
+    apiKey: "AIzaSyDmoyeSgVV4sx1XORlvIunuIHbMkwQmDds",
+    authDomain: "tn-line-hackathon.firebaseapp.com",
+    databaseURL: "https://tn-line-hackathon.firebaseio.com",
+    projectId: "tn-line-hackathon",
+    storageBucket: "tn-line-hackathon.appspot.com",
+    messagingSenderId: "737502471943"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+
+  let result = {}
+  let app = firebase.database().ref('/daily/' + request.query.date);
+    app.on('value', snapshot => {
+      console.log(snapshot.val());
+      result = snapshot.val();
+    });
+
+  response.send(result);
+});
